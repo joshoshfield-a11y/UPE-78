e4117ef69be2d599c5d21a506eb00d1051280430
Taylor Christian Mattheisen <joshoshfield@gmail.com>
2026-07-12 20:52:10 -0700

    Update upe78/__init__.py - add theology and logic modules

--- upe78/__init__.py
+++ upe78/__init__.py
@@ -2,12 +2,12 @@
 UPE-78: Universal Processing Engine 78
 Multi-Agent Acceleration Architecture v10.0
 
-Author: Joshoshfield
-License: MIT
+Author: Taylor Christian Mattheisen
+License: Proprietary Commercial
 """
 
 __version__ = "10.0.0"
-__author__ = "Joshoshfield"
+__author__ = "Taylor Christian Mattheisen"
 
 from .core import UPE78Engine, TensorAccelerator, TopologyEngine
 from .neural import DynamicNeuralNet, ArchitectureSearch, NeuralController
@@ -15,6 +15,8 @@ from .quantum import QuantumLayer, StochasticQuantum, HybridCompute
 from .stochastic import StochasticEngine, ProcessModel, MonteCarlo
 from .ai_control import AgentSwarm, ControlTheory, Orchestrator
 from .symbolic import SymbolicRegistry, OperatorSpace, LensTopology
+from .theology import TheologicalCoherence, OntologicalArgument, AxiomStatus
+from .logic import PropositionalLogic, ModalLogic, FuzzyLogic, TheoremProver
 
 __all__ = [
     "UPE78Engine",
@@ -35,4 +37,11 @@ __all__ = [
     "SymbolicRegistry",
     "OperatorSpace",
     "LensTopology",
+    "TheologicalCoherence",
+    "OntologicalArgument",
+    "AxiomStatus",
+    "PropositionalLogic",
+    "ModalLogic",
+    "FuzzyLogic",
+    "TheoremProver",
 ]
